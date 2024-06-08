001a03179942244c55172e6a43de402d91403f20
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-06-08 14:47:59 +0200

    add validation rules to accointances

--- app/models/accointance.rb
+++ app/models/accointance.rb
@@ -6,11 +6,12 @@ class Accointance < ApplicationRecord
   validates :recipient_id, presence: true
   validates :status, inclusion: { in: %w[pending accepted refused] }
   validate :different_follower_recipient_id
+  validate :unique_acquaintance
 
   def friend_of(current_user)
     current_user == follower ? recipient : follower
-  end 
-  
+  end
+
   private
 
   def default_accointance_status
@@ -19,5 +20,11 @@ class Accointance < ApplicationRecord
 
   def different_follower_recipient_id
     error.add(:base, "The Follower and Recipient must be different") if follower_id == recipient_id
+  end
+
+  def unique_acquaintance
+    existing_acquaintance = Accointance.find_by(follower_id: follower_id, recipient_id: recipient_id) ||
+                            Accointance.find_by(follower_id: recipient_id, recipient_id: follower_id)
+    errors.add(:base, "An acquaintance request already exists for this pair") if existing_acquaintance.present?
   end
 end
