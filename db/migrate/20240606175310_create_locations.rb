1d3da4d9ee4a6080a57605f21ad09b13a9382dcd
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-06-08 10:44:40 +0200

    updated seed with users

--- db/migrate/20240606175310_create_locations.rb
+++ db/migrate/20240606175310_create_locations.rb
@@ -2,6 +2,7 @@ class CreateLocations < ActiveRecord::Migration[7.1]
   def change
     create_table :locations do |t|
       t.string :type
+      t.string :name
       t.string :address
       t.string :price_range
 
