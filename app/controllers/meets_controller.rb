3d1c5bd77d8630cdcce3960f360946c8cce32207
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-06-08 16:32:25 +0200

    added Friends management

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -1,2 +1,26 @@
 class MeetsController < ApplicationController
+  before_action :set_friend
+
+  def new
+    @meet = Meet.new
+  end
+
+  def create
+    @meet = @friend.meets.new(meet_params)
+    if @meet.save
+      redirect_to @meet, notice: 'Meet was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_friend
+    @friend = Accointance.find(params[:friend_id])
+  end
+
+  def meet_params
+    params.require(:meet).permit(:date, :location_type)
+  end
 end
