106e11cd5831925e83d4c4386b9944fa6c08dc7b
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-06-11 19:56:23 +0200

    added seed and updated routs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :friends, only: %i[index show]
+  resources :friends, only: %i[index show new]
   resources :users do
-    resources :accointances, only: %i[create]
+    resources :accointances, only: %i[new create]
   end
   resources :accointances, only: %i[index]
 
