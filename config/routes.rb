50229dda19f5a9450a242e4bdc4064ff173386a0
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-06-11 20:47:44 +0200

    updated passwords for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get '/user' => "friends#index", :as => :user_root
 
   resources :friends, only: %i[index show] do
     resources :meets, only: %i[new create] do
