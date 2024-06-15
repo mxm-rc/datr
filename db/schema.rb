ed0b50375c9ee8725cba1f433cfc006627482cc5
lewagon1540 <christophe.marco@net-c.fr>
2024-06-15 12:03:40 +0200

    3 new tables added to give Category and SubCategory option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_175744) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_091553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_175744) do
     t.datetime "updated_at", null: false
     t.index ["follower_id"], name: "index_accointances_on_follower_id"
     t.index ["recipient_id"], name: "index_accointances_on_recipient_id"
+  end
+
+  create_table "location_categories", force: :cascade do |t|
+    t.bigint "location_id", null: false
+    t.bigint "venue_category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_location_categories_on_location_id"
+    t.index ["venue_category_id"], name: "index_location_categories_on_venue_category_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -86,10 +95,31 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_175744) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "venue_categories", force: :cascade do |t|
+    t.string "main_category"
+    t.string "sub_category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venue_preferences", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "venue_category_id", null: false
+    t.integer "preference_level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_venue_preferences_on_user_id"
+    t.index ["venue_category_id"], name: "index_venue_preferences_on_venue_category_id"
+  end
+
   add_foreign_key "accointances", "users", column: "follower_id"
   add_foreign_key "accointances", "users", column: "recipient_id"
+  add_foreign_key "location_categories", "locations"
+  add_foreign_key "location_categories", "venue_categories"
   add_foreign_key "meets", "accointances"
   add_foreign_key "messages", "accointances"
   add_foreign_key "selected_places", "locations"
   add_foreign_key "selected_places", "meets"
+  add_foreign_key "venue_preferences", "users"
+  add_foreign_key "venue_preferences", "venue_categories"
 end
