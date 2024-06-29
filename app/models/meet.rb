19f4c9a04d984312b32bd03748bea127d294b5ce
Maxime Robert Colin <154891208+mxm-rc@users.noreply.github.com>
2024-06-29 13:07:23 +0200

    updated location model for validation purposes

--- app/models/meet.rb
+++ app/models/meet.rb
@@ -2,4 +2,6 @@ class Meet < ApplicationRecord
   belongs_to :accointance
   has_many :meet_venue_categories
   has_many :venue_categories, through: :meet_venue_categories
+  validates :date, presence: true
+  validates :venue_categories, presence: true
 end
