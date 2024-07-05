3ffa749d50e6199891fe63f11364771c626000f0
huret <huretantoine@gmail.com>
2024-07-05 11:15:52 +0200

    ordre accointance

--- app/controllers/accointances_controller.rb
+++ app/controllers/accointances_controller.rb
@@ -2,8 +2,25 @@ class AccointancesController < ApplicationController
   # before_action :set_users, only: [:index]
 
   def index
-    @users = User.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).sort_by do |user|
+      accointance = current_user.accointance_with(user)
+      if accointance.nil?
+        0
+      else
+        case accointance.status
+        when 'accepted'
+          1
+        when 'pending'
+          2
+        when 'refused'
+          3
+        else
+          4
+        end
+      end
+    end
   end
+
 
   def create
     recipient = User.find(params[:user_id])
